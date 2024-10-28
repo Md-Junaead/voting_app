7c73bc35fd79c6ae765feb2350163f52b080d6f4
Md-Junaead <junaead12364@gmail.com>
2024-10-28 21:45:51 +0600

    Result screen complete

--- lib/Voting App/Screen/result_screen.dart
+++ lib/Voting App/Screen/result_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:voting_app/Voting%20App/Provider/voting_provider.dart';
+import 'package:provider/provider.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final votingProvider = Provider.of<VotingProvider>(context);
+    final isTie = votingProvider.isTie;
+    final winners = votingProvider.winners;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Voting Result"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: votingProvider.candidates.length,
+              itemBuilder: (context, index) {
+                final candidate = votingProvider.candidates[index];
+                final isWinner = winners.contains(candidate);
+                return ListTile(
+                  title: Text(candidate.name),
+                  trailing: Text("Votes:${candidate.votes}"),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
